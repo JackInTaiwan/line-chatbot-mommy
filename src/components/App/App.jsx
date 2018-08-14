aef15e5c48c3680aaff68247349b104c95902430
Cheng <jack@TW-LW80680-M.local>
2018-08-14 12:18:17 +0800

    init reactjs files

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  /*
+   * Component render()
+   * see: https://facebook.github.io/react/docs/reusable-components.html#es6-classes
+   */
+  render() {
+    return (
+      <div>Hello, React !</div>
+    );
+  }
+}
