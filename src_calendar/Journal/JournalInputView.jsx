152ac905311da11aa3f9c05554a6e64799f7717b
jack <jackconvolution@gmail.com>
2018-08-17 10:30:55 +0800

    finish skeleton of JournalPage

--- src_calendar/Journal/JournalInputView.jsx
+++ src_calendar/Journal/JournalInputView.jsx
@@ -6,12 +6,24 @@ import "./styles.css";
 export default class JournalInputView extends Component {
   constructor(props){
     super(props);
+
+    this.state = {
+      selectedUnderline: "",
+    }
   }
 
   render() {
     return(
       <div className="journal-input-view">
-        <Input title={"媽咪體重"} name="mom-weight" />
+        <Input title={"媽咪體重"} tail={"kg"} name="mom-weight" inputWidth={50} parent={this} underlineToggle={this.state.selectedUnderline == "mom-weight"}/>
+        <Input title={"Baby體重"} tail={"g"} name="baby-weight" inputWidth={50} parent={this} underlineToggle={this.state.selectedUnderline == "baby-weight"} />
+        <Input title={"Baby身長"} tail={"cm"} name="baby-height" inputWidth={50} parent={this} underlineToggle={this.state.selectedUnderline == "baby-height"} />
+        <div className="save-button">
+          <div className="save-underline"></div>
+          <div className="save-font-container">
+            <div className="save-font">儲存</div>
+          </div>
+        </div>
       </div>
     );
   }
@@ -22,17 +34,35 @@ export default class JournalInputView extends Component {
 export class Input extends Component {
   constructor(props){
     super(props);
+
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    this.props.parent.setState((prev) => {
+      let newState = prev;
+      newState.selectedUnderline = this.props.name;
+      return newState;
+    })
   }
 
   render() {
     const name = this.props.name;
     const title = this.props.title;
+    const tail = this.props.tail;
+    const inputWidth = this.props.inputWidth;
+    const underlineToggle = this.props.underlineToggle;
+    const fontColor = underlineToggle ? "#dddddd" : "#aaaaaa"
 
     return(
-      <div className="input">
-        <div>{title}</div>
-        <input className="input-block" type="text" name={name}></input>
-      </div>
+      <button className="input" onClick={this.onClick}>
+        <div className="input-title-font">{title}</div>
+        <div className="input-container" style={{width: inputWidth}}>
+          <input className="input-block" type="text" name={name} style={{color: fontColor}}></input>
+          {underlineToggle ? <div className="input-tail" style={{color: "#dddddd"}}>{tail}</div> : null}
+          {underlineToggle ? <div className="input-underline"></div> : null}
+        </div>
+      </button>
     );
   }
 }
