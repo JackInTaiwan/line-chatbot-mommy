a4be975f0849366804b8426eeb3208aca1df78af
jack <jackconvolution@gmail.com>
2018-08-17 17:08:25 +0800

    fix bog: cate-button

--- src_calendar/Calendar/FloatMenu.jsx
+++ src_calendar/Calendar/FloatMenu.jsx
@@ -12,23 +12,23 @@ export default class FloatMenu extends Component {
     return(
       <div className="float-menu">
         <div className="float-panel" style={{height: windowHeight}}></div>
-        <div className="cateBlock">
+        <div className="cate-block">
           <Link 
             className="route-link"
             to={{pathname:"/journal", state:{date: selectedDate}}}
           >
             <div className="cate-container">
-              <button className="cateButton">媽媽手冊</button>
+              <button className="cate-button">媽媽手冊</button>
             </div>
           </Link>
           <Link to="/reminder" className="route-link">
             <div className="cate-container">
-              <button className="cateButton">代辦事項</button>
+              <button className="cate-button">代辦事項</button>
             </div>
           </Link>
           <Link to="/diary" className="route-link">
             <div className="cate-container">
-              <button className="cateButton">日記</button>
+              <button className="cate-button">日記</button>
             </div>
           </Link>
         </div>
